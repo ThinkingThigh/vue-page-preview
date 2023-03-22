fd2c844785eb54962b28550a7dc43e6a5e2b1982
liuyi03 <liuyi03@tal.com>
2023-03-22 17:54:09 +0800

    feat: 导航滚动定位实现

--- examples/App.vue
+++ examples/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
     <div @click="handleClick" style="cursor: pointer">点击预览</div>
-    <page-preview ref="page-preview" :val.sync="val" default="1.3" max="2" min="0.9">
+    <page-preview
+      ref="page-preview"
+      :val.sync="val"
+      default="1.3"
+      max="2"
+      min="0.9"
+    >
       <div class="content-container">
         <div class="item" v-for="item in list" :key="item.title">
           <div class="title">{{ item.title }}</div>
@@ -10,7 +16,22 @@
         </div>
       </div>
 
-      <template v-slot:index> index </template>
+      <template v-slot:index>
+        <div class="index-container">
+          <div class="index-item-container">
+            <div
+              :class="['index-item', active == index && 'active']"
+              v-for="(item, index) in list"
+              :key="item.title"
+              @click="handleClickIndex(index)"
+            >
+              <el-tooltip effect="dark" :content="item.title" placement="right">
+                <span class="index-title">{{ item.title }}</span>
+              </el-tooltip>
+            </div>
+          </div>
+        </div>
+      </template>
     </page-preview>
   </div>
 </template>
@@ -84,34 +105,136 @@ export default {
             "一部魔幻小说系列，讲述了哈利·波特等人在霍格沃茨魔法学校的冒险故事。充满了神秘、幻想和冒险的元素，同时也涉及到了友情、家庭等多个方面的主题。",
         },
       ],
+      active: 0,
     };
   },
   methods: {
     handleClick() {
       this.$refs["page-preview"].show = true;
+      setTimeout(() => {
+        // 监听滚动条
+        let dom = document.querySelector(".preview-container");
+        dom.addEventListener("scroll", this.handleScroll);
+      }, 0);
+    },
+    handleClickIndex(index) {
+      let doms = document.querySelectorAll(`.content-container .item`);
+      console.log("handleClickIndex", index);
+      doms[index].scrollIntoView({ behavior: "smooth" });
+      this.active = index;
+    },
+    // 激活左侧对应的导航条
+    activeNavNode() {
+      const doms = document.querySelectorAll(`.content-container .item`);
+      let scrollTop =
+        document.querySelector(".preview-container").scrollTop + 2; // 一些精度问题
+      let domOffsetTops = [];
+      let zoom = this.val;
+      for (let i = 0; i < doms.length; i++) {
+        domOffsetTops.push(doms[i].offsetTop * zoom);
+      }
+
+      if (scrollTop < domOffsetTops[0]) {
+        this.active = 0;
+        return;
+      }
+      if (scrollTop > domOffsetTops[domOffsetTops.length - 1]) {
+        this.active = domOffsetTops.length - 1;
+        return;
+      }
+      for (let i = 0; i < domOffsetTops.length; i++) {
+        if (scrollTop > domOffsetTops[i] && scrollTop < domOffsetTops[i + 1]) {
+          this.active = i;
+          break;
+        }
+      }
+    },
+    handleScroll() {
+      // 防抖动处理
+      clearTimeout(this.timeout);
+      this.timeout = setTimeout(() => {
+        this.activeNavNode();
+      }, 100);
     },
   },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" scoped>
 .content-container {
   display: flex;
   flex-direction: column;
   font-size: 14px;
+  .item {
+    margin-bottom: 14px;
+    .title {
+      font-weight: bold;
+    }
+  }
 }
-.item {
-  margin-bottom: 14px;
-}
-.item .title {
-  font-weight: bold;
+
+.index-container {
+  display: flex;
+  font-size: 18px;
+  color: #a6a6a6;
+  &::before {
+    display: block;
+    content: "";
+    width: 1px;
+    background-color: #a6a6a6;
+    transform: scaleY(0.95);
+  }
+  .index-item-container {
+    display: flex;
+    flex-direction: column;
+    cursor: pointer;
+
+    .index-item {
+      display: flex;
+      align-items: center;
+      margin-left: -4px;
+      margin-bottom: 18px;
+
+      .index-title {
+        display: block;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        width: 90px;
+        font-size: 10px;
+      }
+      &:last-child {
+        margin-bottom: 0;
+      }
+      &:before {
+        display: block;
+        width: 7px;
+        height: 7px;
+        border-radius: 50%;
+        background: #a6a6a6;
+        content: "";
+        margin-right: 10px;
+      }
+      &:hover {
+        color: #1989fa;
+        &:before {
+          display: block;
+          width: 7px;
+          height: 7px;
+          border-radius: 50%;
+          background: #1989fa;
+          content: "";
+          margin-right: 10px;
+          z-index: 99;
+        }
+      }
+    }
+    .active {
+      color: #1989fa;
+      &:before {
+        background: #1989fa;
+      }
+    }
+  }
 }
 </style>
